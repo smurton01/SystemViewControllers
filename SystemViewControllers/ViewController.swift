716cd000936fe910447a980439a591b977fa6148
Saya Murton <saya.murton@gmail.com>
2017-09-15 13:48:52 -0600

    Initial Commit

---
+++ SystemViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Saya Murton on 9/15/17.
+//  Copyright © 2017 Saya Murton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
