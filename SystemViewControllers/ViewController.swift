e28e8ee66bf67e9da01bbdebc2a1e91eea6b46fb
Saya Murton <saya.murton@gmail.com>
2017-09-15 19:58:56 -0600

    More code for `@IBAction func photosButtonTapped`
    -added cameraAction & photoLibraryAction

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -31,7 +31,18 @@ class ViewController: UIViewController {
         let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {
+            action in print("User selected Camera action")
+        })
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: {
+            action in print("User selected Photo Library action")
+        })
+        
         alertController.addAction(cancelAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
         alertController.popoverPresentationController?.sourceView = sender
     
         present(alertController, animated: true, completion: nil)
