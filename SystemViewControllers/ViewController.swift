d77617c1e994f78d617cbd71a4ce49bd24ef23de
Saya Murton <saya.murton@gmail.com>
2017-09-18 11:41:10 -0600

    Access photo library

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBAction func emailButtonTapped(_ sender: Any) {
+    }
     @IBAction func shareButtonTapped(_ sender: UIButton) {
         guard let image = imageView.image else { return }
         let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
@@ -28,40 +30,41 @@ class ViewController: UIViewController {
     }
     
     @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
         let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
-        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {
-            action in print("User selected Camera action")
-        })
-        
-        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: {
-            action in print("User selected Photo Library action")
-        })
-        
         alertController.addAction(cancelAction)
-        alertController.addAction(cameraAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+        })
+            
+            alertController.addAction(cameraAction)
+            
+        }
+        
+        if
+        UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true, completion: nil)
+        })
+
         alertController.addAction(photoLibraryAction)
-        alertController.popoverPresentationController?.sourceView = sender
     
         present(alertController, animated: true, completion: nil)
     }
     
-    @IBAction func emailButtonTapped(_ sender: Any) {
     }
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) { if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        imageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
